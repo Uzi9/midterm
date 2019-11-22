e095910f28b71546f6b3c8750e845ba3db9621a7
Uzi9 <1711595163@qq.com>
2019-11-22 00:48:09 -0800

    added a file

---
+++ EX1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    printf("请输入一段总长度不超过50个字符的任意字母数字，空格符，制表符\n");
+    char c[50];
+    int i = 0;
+    int j = 1;
+    int k = 0;
+    while((c[i]= getchar()) != '\n'){
+        i++;
+    }
+    for(k = 0;k<i;){
+        if((c[k]>='A'&&c[k]<='Z')||(c[k]>='a'&&c[k]<='z')){
+            k++;
+        }
+        
+        else{
+            j++;
+            k++;
+        
+        }
+    }
+    printf("The number of word is: %d\n",j);
+    return 0;
+}
